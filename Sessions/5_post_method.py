dcd68dbb636430664fb32d8b5a3cbfdbf1ef8dd0
Rahul Singh <rahulkumar21work@gmail.com>
2026-09-20 17:05:42 +0530

    post-api

---
+++ Sessions/5_post_method.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI
+@app.post("/create-user")
+def create_user(name:str, age:int):   # isme url query param ki tarah aata hai
+    return {                          
+        "Name": name,
+        "Age": age
+    }
+
+@app.post("/create-user")            # isme url normal aata hai
+def create_user(user:dict):          # request body m json data bhejna    
+    return {
+        "message": "User Created",
+        "Data": user
+    }
