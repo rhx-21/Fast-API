b76f0c9348897d4532fe0b29f4df51a61d40b7db
Rahul Singh <rahulkumar21work@gmail.com>
2026-09-20 17:29:44 +0530

    pydantic-model

---
+++ Sessions/6_pydantic.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Address(BaseModel):
+    city: str
+    pincode: int
+
+class User(BaseModel):
+    name: str
+    age: int
+    address: Address
+
+@app.post("/create-user")
+def create_user(user:User):
+    return {
+        "message": "User Created",
+        "Data": user
+    }
