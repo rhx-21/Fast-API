fa31d34601f166b40728d475ccfd48ae850263f5
Rahul Singh <rahulkumar21work@gmail.com>
2026-09-21 18:59:46 +0530

    describe-sqlalchemy-code

--- Sessions/16_sqlalchemy.py
+++ Sessions/16_sqlalchemy.py
@@ -1,4 +1,5 @@
 # pip install sqlalchemy
+# ORM - Database ko python code k through handle karne ka tareeka hai
 
 from sqlalchemy import create_engine,Column,Integer,String
 from sqlalchemy.orm import sessionmaker,declarative_base,Session
@@ -6,6 +7,7 @@ from fastapi import FastAPI,Depends
 
 app = FastAPI()
 
+# Database URL
 DATABASE_URL = "sqlite:///./test.db"
 
 # connect database
@@ -14,12 +16,13 @@ engine = create_engine(
     connect_args={"check_same_thread":False}
 )
 
-# interact with db (operation handle in db)
+# Create Sessions - interact with db (operation handle in db)
 sessionLocal = sessionmaker(bind=engine)
 
 # Model banane k liye base
 Base = declarative_base()
 
+# Table (Model)
 class Todo(Base):
     __tablename__ = "Todos"
     
@@ -27,8 +30,10 @@ class Todo(Base):
     title = Column(String)
     completed = Column(String)
     
+# Table Create
 Base.metadata.createall(bind=engine)
 
+# Dependency(DB Session Provide Karega)
 def get_db():
     db = sessionLocal()
     try:
