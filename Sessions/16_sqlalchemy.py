4ee96ccfcadcc36b40c4e79c5d858ddd0f50345e
Rahul Singh <rahulkumar21work@gmail.com>
2026-09-21 17:31:45 +0530

    sqlalchemy

---
+++ Sessions/16_sqlalchemy.py
@@ -0,0 +1,45 @@
+# pip install sqlalchemy
+
+from sqlalchemy import create_engine,Column,Integer,String
+from sqlalchemy.orm import sessionmaker,declarative_base,Session
+from fastapi import FastAPI,Depends
+
+app = FastAPI()
+
+DATABASE_URL = "sqlite:///./test.db"
+
+# connect database
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread":False}
+)
+
+# interact with db (operation handle in db)
+sessionLocal = sessionmaker(bind=engine)
+
+# Model banane k liye base
+Base = declarative_base()
+
+class Todo(Base):
+    __tablename__ = "Todos"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    completed = Column(String)
+    
+Base.metadata.createall(bind=engine)
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get('/')
+def home(db: Session = Depends(get_db)):
+    return{
+        "message":"DB Connected"
+    } 
+
+
