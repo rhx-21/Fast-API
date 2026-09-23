8a02983c8a0b178d8778d57ada8ab1755527547c
Rahul Singh <rahulkumar21work@gmail.com>
2026-09-23 11:05:47 +0530

    Config-and-env-file

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    ORIGINS = os.getenv("ORIGINS") 
+    DB_URL = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+settings = Settings()
